5b41ebef371fdf51501768300491fce999438687
Uladzimir Bely <vladzimir.bely@gmail.com>
2024-04-04 10:00:14 +0300

    refactor code, add recource.py file, delete import

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,5 +1,5 @@
-import os
 from selene import browser, command, have
+import resource
 
 class RegistrationPage:
     def open(self):
@@ -17,8 +17,8 @@ class RegistrationPage:
     def fill_number(self, number):
         browser.element('#userNumber').type(number)
 
-    def fill_gender(self, value):
-        browser.element(value).click()
+    def fill_gender(self):
+        browser.element('[for="gender-radio-2"]').click()
 
     def fill_date_birth(self, month, year, day):
         browser.element('#dateOfBirthInput').click()
@@ -29,11 +29,11 @@ class RegistrationPage:
     def fill_subjects(self, value):
         browser.element('#subjectsInput').type(value).press_enter()
 
-    def fill_hobbies(self, value):
-        browser.element(value).click()
+    def fill_hobbies(self):
+        browser.element('[for="hobbies-checkbox-3"]').click()
 
     def upload_photo(self, path):
-        browser.element('#uploadPicture').send_keys(os.path.abspath(path))
+        browser.element('#uploadPicture').send_keys(resource.path(path))
 
     def fill_address(self, value):
         browser.element('#currentAddress').type(value)
@@ -45,18 +45,18 @@ class RegistrationPage:
     def click_submit(self):
         browser.element('#submit').perform(command.js.scroll_into_view).click()
 
-    def should_have_registered(self, full_name, email, gender, number, date_birth, subjects, hobbies, photo, state, city):
+    def should_have_registered(self, full_name, email, gender, number, date_birth, subjects, hobbies, photo, address,
+                               state_city):
         browser.element('.table').all('tr td:nth-child(2)').should(have.texts
             (
-            'first name last name',
-            'test@test.test',
-            'Female',
-            '1234567890',
-            '02 March,1998',
-            'Computer Science',
-            'Music',
-            'Screenshot_1.png',
-            'Address',
-            'Haryana Karnal'
+            full_name,
+            email,
+            gender,
+            number,
+            date_birth,
+            subjects,
+            hobbies,
+            photo,
+            address,
+            state_city
         ))
-
